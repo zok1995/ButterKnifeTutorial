06a1ca2ba57cdaa9ee160de527cfb23ca2bee691
Oleksandr Malakhovskyi <zokzok95@gmail.com>
2016-03-10 22:49:58 +0200

    Fragment and ButterKnife tutorial

---
+++ MyApplication2/app/src/main/java/com/example/oleksandr/justklicker/Fragment1.java
@@ -0,0 +1,9 @@
+package com.example.oleksandr.justklicker;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by oleksandr on 10.03.16.
+ */
+public class Fragment1 extends Fragment{
+}
--- MyApplication2/app/src/main/java/com/example/oleksandr/justklicker/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/oleksandr/justklicker/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.oleksandr.justklicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity  {
+    @Bind(R.id.textView) TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        textView.setText("alalal long");
     }
+    @OnClick(R.id.button)
+    public void submit(){
+        Toast.makeText(getApplicationContext(),"WORKING",Toast.LENGTH_LONG).show();
+    }
+
+
 }
