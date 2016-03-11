688ee0fa0f3a33620fcba9ec06bd0b17049bfc31
Oleksandr Malakhovskyi <zokzok95@gmail.com>
2016-03-11 21:50:07 +0200

    Fragment 2 added

--- MyApplication2/app/src/main/java/com/example/oleksandr/justklicker/Fragment2.java
+++ MyApplication2/app/src/main/java/com/example/oleksandr/justklicker/Fragment2.java
@@ -1,9 +1,14 @@
 package com.example.oleksandr.justklicker;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 /**
  * Created by oleksandr on 10.03.16.
  */
 public class Fragment2 extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
--- MyApplication2/app/src/main/java/com/example/oleksandr/justklicker/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/oleksandr/justklicker/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.oleksandr.justklicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
